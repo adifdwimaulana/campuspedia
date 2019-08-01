e75b2c2a99b863e5fa5c6c3f7814c3d24d1e7f24
Andhika <andhikay24@gmail.com>
2019-08-01 16:31:20 +0700

    setting validation ID

--- resources/views/admin/jobs/perusahaan.blade.php
+++ resources/views/admin/jobs/perusahaan.blade.php
@@ -11,6 +11,20 @@
     </div>
 
     <div class="page-content">
+      <div class="col-12 mb-2 mt-2">
+        @if ($errors->any())
+            @foreach ($errors->all() as $error)
+                <div class="alert alert-danger" role="alert">
+                    {{ $error }}
+                </div>
+            @endforeach  
+        @endif
+        @if(session()->has('message'))
+            <div class="alert alert-success" role="alert">
+                <strong>Sukses!</strong> {{ session()->get('message') }}
+            </div>
+        @endif
+      </div>
       <div class="panel">
         <div class="panel-body container-fluid">
           <div class="row row-lg">
